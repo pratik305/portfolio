16eab920f085a974b54a049ff94095e5b01f5775
Pratik Jadhav <pratikjivanjadhav77@gmail.com>
2026-06-25 22:08:33 +0530

    Remove phone number from contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -14,16 +14,6 @@ const contactDetails = [
     ),
   },
   {
-    label: 'Phone',
-    value: '+91 9511650456',
-    href: 'tel:+919511650456',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 13a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-      </svg>
-    ),
-  },
-  {
     label: 'LinkedIn',
     value: 'pratikjadhavlink',
     href: 'https://www.linkedin.com/in/pratikjadhavlink/',
